adda19c99fafc4f97eb5616d844518903d685233
harsit09 <84085808+harsit09@users.noreply.github.com>
2023-03-11 21:51:27 +0530

    Create esc_char.pl

---
+++ esc_char.pl
@@ -0,0 +1,14 @@
+#use of q  
+use strict;  
+use warnings;  
+my $x = 'tutorials';  
+print q(Our site "javaTpoint" provides all type of "$x"\n);  
+print"\n";  
+#use of () brackets  
+print q(Our site "javaTpoint" provides all type of "$x"\n);  
+print"\n";  
+#use of {} brackets  
+print q{Our site )javaTpoint( provides all type of "$x"\n};  
+print"\n";  
+#use of {} brackets  
+print q[Our site )javaTpoint} provides all type of "$x"\n];  
